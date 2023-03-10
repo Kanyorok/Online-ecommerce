c406a8c451d7a4725052bea2215382bad9c253a7
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-03-10 13:37:05 +0300

    create and save product to database successful

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,32 @@
+const Product = require('../models/product');
+
+// Create a new Product => /api/products/new
+exports.newProduct = async (req, res, next) => {
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        message: 'Product created successfully.',
+        product
+    })
+
+    // try {
+    //     const savedProduct = await product.save();
+    //     res.status(201).json({
+    //         success: true,
+    //         message: 'Product created successfully.',
+    //         product: savedProduct
+    //     })
+    // } catch (err) {
+    //     res.status(400).json({
+    //         success: false,
+    //         message: 'Roberto is great',
+    //         error: err
+    //     })
+    // }
+}
+
+
 exports.getProducts = (req, res, next) => {
     res.status(200).json({
         success: true,
