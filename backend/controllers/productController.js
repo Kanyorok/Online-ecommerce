ecd369225c60cc8d4dd66efb27dcb70be09602cb
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-03-10 20:34:05 +0300

    Renamed product.json to Products.json`````Made a util script for adding products.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,28 +2,21 @@ const Product = require('../models/product');
 
 // Create a new Product => /api/products/new
 exports.newProduct = async (req, res, next) => {
-    const product = await Product.create(req.body);
-
-    res.status(201).json({
-        success: true,
-        message: 'Product created successfully.',
-        product
-    })
-
-    // try {
-    //     const savedProduct = await product.save();
-    //     res.status(201).json({
-    //         success: true,
-    //         message: 'Product created successfully.',
-    //         product: savedProduct
-    //     })
-    // } catch (err) {
-    //     res.status(400).json({
-    //         success: false,
-    //         message: 'Roberto is great',
-    //         error: err
-    //     })
-    // }
+   
+    try {
+        const product = await Product.create(req.body);
+        res.status(201).json({
+            success: true,
+            message: 'Product created successfully.',
+            product
+        })
+    } catch (err) {
+        res.status(400).json({
+            success: false,
+            message: 'Roberto is great',
+            error: err
+        })
+    }
 }
 
 
