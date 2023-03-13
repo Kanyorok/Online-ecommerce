0ae19792eef910d47f5238f3f0a63564bf8a5066
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-03-13 13:11:20 +0300

    created functionalities for updating products in database using a the id.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product');
 
-// Create a new Product => /api/products/new
+// Create a new Product => /api/v1/admin/product/new
 exports.newProduct = async (req, res, next) => {
    
     try {
@@ -13,7 +13,7 @@ exports.newProduct = async (req, res, next) => {
     } catch (err) {
         res.status(400).json({
             success: false,
-            message: 'Roberto is great',
+            message: 'The product was not created.',
             error: err
         })
     }
@@ -49,4 +49,28 @@ exports.getSingleProduct = async (req, res, next) => {
         message: 'Product found',
         product
     })
+}
+
+// Update product => /api/v1/admin/product/:id
+exports.updateProduct = async (req, res, next) => {
+
+    let product = await Product.findByIdAndUpdate(req.params.id)
+
+    if(!product){
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        userFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true
+    })
+
 }
