f0307010a578a7155546c7bdbaa0a74b1ed13c8f
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-03-13 14:48:58 +0300

    created error handler class. They work in production. However, there needs separation between production and development errors.

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const errorMiddleware = require('./middlewares/errors')
 const app = express();
 
 app.use(express.json())
@@ -8,5 +9,8 @@ const products = require('./routes/product');
 
 
 app.use('/api/v1', products)
+
+// Middleware to handle errors.
+app.use(errorMiddleware)
 
 module.exports = app
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const ErrorHandler = require('../utils/errorHandler');
 
 // Create a new Product => /api/v1/admin/product/new
 exports.newProduct = async (req, res, next) => {
@@ -38,10 +39,7 @@ exports.getSingleProduct = async (req, res, next) => {
     const product = await Product.findById(req.params.id)
 
     if(!product){
-        return res.status(404).json({
-            success: false,
-            message: 'Product not found'
-        })
+        return next(new ErrorHandler('Product not found', 404))
     }
 
     res.status(404).json({
