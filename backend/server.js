ea139d923c7afa8d3be81366feba8c9e74b13077
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-03-14 15:42:34 +0300

    Created function to take care of unhandled promise rejections like 'invalid strings'

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,14 @@ dotenv.config({path: 'backend/config/config.env'})
 connectDatabase();
 
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} node.`)
+})
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', err => {
+    console.log(`Error: ${err.message}`);
+    console.log('Shutting down the server due to unhandled promise rejection');
+    // Close server & exit process
+    server.close(() => process.exit(1));
 })
