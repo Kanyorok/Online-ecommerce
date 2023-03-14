a13e89cb40be2b88ee5ec825766415dc195f08aa
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-03-14 12:59:13 +0300

    Separated production and development errors using the errors script in utils. Nonetheless, check the low threat error to the database:  "Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client"

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -2,7 +2,31 @@ const ErrorHandler = require('../utils/errorHandler');
 
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
-    err.message = err.message || 'internal server error';
+   
+    if(process.env.NODE_ENV === 'DEVELOPMENT'){
+        res.status(err.statusCode).json({
+            success: false,
+            error: err,
+            errMessage: err.message,
+            stack: err.stack
+        })
+    }
+
+    if(process.env.NODE_ENV === 'PRODUCTION'){
+
+        let error = {...err}
+
+        error.message = err.message
+
+        res.status(err.statusCode).json({
+            success: false,
+            error: error.message || 'internal server error'
+        })
+
+
+    }
+
+    
 
     res.status(err.statusCode).json({
         success: false,
