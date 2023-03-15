3a37c21253720b29bd3731c9c5c5175030f491d8
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-03-15 16:01:08 +0300

    Created script to login user based on the correctly applicable token.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser } = require('../controllers/authcontroller');
+const { registerUser, loginUser } = require('../controllers/authcontroller');
 
 router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
 
 module.exports = router;
