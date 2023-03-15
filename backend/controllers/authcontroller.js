7b9dda9a7a134ad3ea81211172c80f3cfc96b08a
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-03-15 17:01:16 +0300

    Created script to send JWT token in cookie.

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncError = require('../middlewares/catchAsyncErrors');
+const sendToken = require('../utils/jwtToken')
 
 // Register a user => /api/v1/register
 exports.registerUser = catchAsyncError(async (req, res, next) => {
@@ -15,12 +16,7 @@ exports.registerUser = catchAsyncError(async (req, res, next) => {
         }
     })
 
-    const token = user.getJwtToken();
-
-    res.status(201).json({
-        success: true,
-        token
-    })
+    sendToken(user, 200, res)
 })
 
 // Login a user => /api/v1/login
@@ -46,9 +42,6 @@ exports.loginUser = catchAsyncError(async(req, res, next) => {
         return next(new ErrorHandler('Invalid Email or Password', 401));
     }
 
-    const token = user.getJwtToken();
-    res.status(200).json({
-        success: true,
-        token
-    })
+    sendToken(user, 200, res)
+
 })
