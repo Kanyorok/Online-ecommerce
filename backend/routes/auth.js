a50e6a04cf1a33e11ede89c080e368a134ba69b8
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-03-16 11:52:24 +0300

    Logging out the user.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/authcontroller');
+const { registerUser, loginUser, logout } = require('../controllers/authcontroller');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+router.route('/logout').get(logout);
 
 module.exports = router;
