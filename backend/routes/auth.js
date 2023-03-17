ec12a45d3c04390bd0be168dd37d9e9fab88c746
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-03-17 15:13:35 +0300

    Delete the user from admin side privilege only to admin roles.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,7 +9,9 @@ const { registerUser,
         updatePassword,
         updateProfile,
         allUsers,
-        getUserDetails} = require('../controllers/authcontroller');
+        getUserDetails,
+        updateUser,
+        deleteUser} = require('../controllers/authcontroller');
 const { isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth')
 
 
@@ -22,6 +24,9 @@ router.route('/me').get(isAuthenticatedUser, getUserProfile);
 router.route('/password/update').put(isAuthenticatedUser, updatePassword);
 router.route('/me/update').put(isAuthenticatedUser, updateProfile);
 router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers);
-router.route('/admin/user/:id').get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails)
+router.route('/admin/user/:id')
+    .get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails)
+    .put(isAuthenticatedUser, authorizeRoles('admin'), updateUser)
+    .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteUser);
 
 module.exports = router;
