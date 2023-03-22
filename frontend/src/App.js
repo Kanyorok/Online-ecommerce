3d0758c61fbd9a12e9623b13edfe9b312896ef3f
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-03-22 20:01:52 +0300

    implemented the react router for navigation.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,23 @@
-import Header from './components/layout/Header';
-import Footer from './components/layout/Footer';
-import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import Home from "./components/Home";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container container-fluid">
+          <Routes>
+            <Route path="/" Component={Home} exact />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
