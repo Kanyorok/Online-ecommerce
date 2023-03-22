4e9044a92f85f38290347398f144ce95425ff1f3
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-03-22 18:06:05 +0300

    Made the homepage of the ecommerce site.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import Home from './components/Home';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
       <Footer/>
     </div>
   );
