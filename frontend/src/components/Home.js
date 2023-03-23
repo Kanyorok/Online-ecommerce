f7161ecec4973b7f31a00902ad2eca3c6ea8fe7f
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-03-23 17:06:41 +0300

    Encountered major error with react-alert-template-basic. Unfortunately, I have not managed to get the prompt messages to appear.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,36 +1,49 @@
 import React, { Fragment, useEffect } from "react";
 import MetaData from "./layout/MetaData";
 import { useDispatch, useSelector } from "react-redux";
+import { useAlert } from "react-alert";
 import { getProducts } from "../actions/productAction";
 import Product from "./product/Product";
+import Loader from "./layout/Loader";
+
+
+
 
 const Home = () => {
+  const alert = useAlert();
   const dispatch = useDispatch();
 
-  const { loading, products, error, productCount } = useSelector(
-    (state) => state.products
-  );
+  const { loading, products, error, productCount } = useSelector(state => state.products);
 
   useEffect(() => {
+
+    if (error) {
+      alert.success=('Success')
+      return alert.error(error)
+    }
+
     dispatch(getProducts());
-  }, [dispatch]);
+
+  }, [dispatch, alert, error]);
 
   return (
     <Fragment>
-      {loading ? <h1>Loading....</h1>: (
-      <Fragment>
-        <MetaData title={" Buy Best Products Online"} />
-        <h1 id="products_heading">Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title={" Buy Best Products Online"} />
+          <h1 id="products_heading">Latest Products</h1>
 
-        <section id="products" className="container mt-5">
-          <div className="row">
-            {products &&
-              products.map((product) => (
-                < Product key={product._id} product={product} />
-              ))}
-          </div>
-        </section>
-      </Fragment>
+          <section id="products" className="container mt-5">
+            <div className="row">
+              {products &&
+                products.map((product) => (
+                  <Product key={product._id} product={product} />
+                ))}
+            </div>
+          </section>
+        </Fragment>
       )}
     </Fragment>
   );
