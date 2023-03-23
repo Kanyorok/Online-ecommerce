9820236e815ba5fa4d769235e632a0d22a04cb99
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-03-23 09:32:08 +0300

    BE CAREFUL: Commit contains adjustments for productController. The pagination was added and forked under this commit. Also, the reducers for products were integrated.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -13,7 +13,7 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
         res.status(201).json({
             success: true,
             message: 'Product created successfully.',
-            product
+            product  
         })
     } catch (err) {
         res.status(400).json({
@@ -27,13 +27,19 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 // Get all products => /api/v1/products?keyword=apple
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
     
-    const apiFeatures = new APIFeatures(Product.find(), req.query).search().filter()
+    const resPerPage = 5;
+    const productCount = await Product.countDocuments()
+
+    const apiFeatures = new APIFeatures(Product.find(), req.query)
+            .search()
+            .filter()
+            .pagination(resPerPage);
 
     const products = await apiFeatures.query;
     
     res.status(200).json({
         success: true,
-        count: products.length,
+        productCount,
         products
     })
 })
