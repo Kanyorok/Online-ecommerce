5f15a41211b4a8d6c9ee121260cc0a4392a08ee1
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-03-31 14:36:41 +0300

    search by ratings functionality.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -7,54 +7,58 @@ import { getProducts } from "../actions/productAction";
 import Product from "./product/Product";
 import Loader from "./layout/Loader";
 import { useParams } from "react-router-dom";
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css'
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 const { createSliderWithTooltip } = Slider;
 const Range = createSliderWithTooltip(Slider.Range);
 
 const Home = () => {
-
   const alert = useAlert();
   const dispatch = useDispatch();
-  const { loading, error, products, productsCount, resPerPage, filteredProductsCount } = useSelector((state) => state.products)
+  const {
+    loading,
+    error,
+    products,
+    productsCount,
+    resPerPage,
+    filteredProductsCount,
+  } = useSelector((state) => state.products);
   const { keyword } = useParams();
 
-  const Keyword = keyword
+  const Keyword = keyword;
 
   const [currentPage, setCurrentPage] = useState(1);
-  const [price, setPrice] = useState([100, 10000])
-  const [category, setCategory] = useState('')
+  const [price, setPrice] = useState([100, 10000]);
+  const [category, setCategory] = useState("");
+  const [rating, setRating] = useState(0);
 
   const categories = [
-    'Electronics', 
-    'Cameras', 
-    'Laptops', 
-    'Accessories', 
-    'Headphones', 
-    'Food', 
-    'Clothes', 
-    'Shoes'
-  ]
+    "Electronics",
+    "Cameras",
+    "Laptops",
+    "Accessories",
+    "Headphones",
+    "Food",
+    "Clothes",
+    "Shoes",
+  ];
 
   useEffect(() => {
-
     if (error) {
-      return alert.error(error)
+      return alert.error(error);
     }
 
-    dispatch(getProducts(Keyword, currentPage, price, category));
-
-
-  }, [dispatch, Keyword, category, alert, currentPage, error, price])
+    dispatch(getProducts(Keyword, currentPage, price, category, rating));
+  }, [dispatch, Keyword, category, alert, currentPage, error, price, rating]);
 
   function setCurrentPageNo(pageNumber) {
-    setCurrentPage(pageNumber)
+    setCurrentPage(pageNumber);
   }
 
   let count = productsCount;
-  if(Keyword){
-    count = filteredProductsCount
+  if (Keyword) {
+    count = filteredProductsCount;
   }
 
   return (
@@ -89,43 +93,61 @@ const Home = () => {
                         onChange={(price) => setPrice(price)}
                       />
 
-                      <hr className="mt-5"/>
+                      <hr className="my-5" />
 
                       <div className="mt-5">
                         <h4 className="mb-3"> Categories</h4>
 
                         <ul className="pl-0">
-                            {categories.map(category => (
-                                <li
-                                  style={{cursor: 'pointer',
-                                listStyle: 'none'}}
-                                key={category}
-                                onClick = {() => setCategory(category)}
-                                >
-                                  {category}
-                                </li>
-
-                              ))}
+                          {categories.map((category) => (
+                            <li
+                              style={{ cursor: "pointer", listStyle: "none" }}
+                              key={category}
+                              onClick={() => setCategory(category)}
+                            >
+                              {category}
+                            </li>
+                          ))}
                         </ul>
                       </div>
+                      <hr className="my-5" />
 
+                      <div className="mt-5">
+                        <h4 className="mb-3"> Ratings </h4>
 
+                        <ul className="pl-0">
+                          {[5, 4, 3, 2, 1].map((star) => (
+                            <li
+                              style={{ cursor: "pointer", listStyle: "none" }}
+                              key={star}
+                              onClick={() => setRating(star)}
+                            >
+                              <div className="rating-outer">
+                                <div
+                                  className="rating-inner"
+                                  style={{
+                                    width: `${star * 20}%`,
+                                  }}
+                                ></div>
+                              </div>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
                     </div>
                   </div>
 
                   <div className="col-6 col-md-9">
                     <div className="row">
-                      {
-                        products.map((product) => (
-                          <Product key={product._id} product={product} col={5}/>
-                        ))
-                      }
+                      {products.map((product) => (
+                        <Product key={product._id} product={product} col={4} />
+                      ))}
                     </div>
                   </div>
                 </Fragment>
               ) : (
                 products.map((product) => (
-                  <Product key={product._id} product={product} col={4}/>
+                  <Product key={product._id} product={product} col={4} />
                 ))
               )}
             </div>
