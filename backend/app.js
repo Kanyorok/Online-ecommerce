5cba1171d0426a37cdfba66061eddc7904e1dcd5
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-04-01 10:54:23 +0300

    made changes to backend as compared to previous commit: -> moved the cloudinary.config to server.js. -> installed the express-fileupload package. -> included the package in the app.js under app.use fileupload.

--- backend/app.js
+++ backend/app.js
@@ -3,18 +3,13 @@ const errorMiddleware = require('./middlewares/errors')
 const app = express();
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
-const cloudinary = require('cloudinary')
+const fileUpload = require('express-fileupload')
+
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
-
-// Setting up cloudinary configuration
-cloudinary.config({
-    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
+app.use(fileUpload())
 
 //import all the routes
 const products = require('./routes/product');
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const app = require('./app')
 const connectDatabase = require('./config/database')
-
+const cloudinary = require('cloudinary')
 const dotenv = require('dotenv');
+
 
 // Handle the uncaught exceptions 
 process.on('uncaughtException', err => {
@@ -17,6 +18,13 @@ dotenv.config({path: 'backend/config/config.env'})
 
 // connecting to database
 connectDatabase();
+
+// Setting up cloudinary configuration
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 
 const server = app.listen(process.env.PORT, () => {
