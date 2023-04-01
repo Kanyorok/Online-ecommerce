6324d75ed0e6eede1ca4773035b75f3deff92bb2
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-04-01 17:51:33 +0300

    The Load user functionality for the Ecommerce.

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -4,6 +4,9 @@ import { LOGIN_REQUEST,
          REGISTER_USER_REQUEST,
          REGISTER_USER_SUCCESS,
          REGISTER_USER_FAIL,
+         LOAD_USER_REQUEST,
+         LOAD_USER_SUCCESS,
+         LOAD_USER_FAIL,
          CLEAR_ERRORS } from "../constants/userConstants"
 
 
@@ -12,6 +15,7 @@ export const authReducer = (state = {user: {}}, action) => {
 
         case LOGIN_REQUEST:
         case REGISTER_USER_REQUEST:
+        case LOAD_USER_REQUEST:
             return {
                 loading: true,
                 isAuthenticated: false,
@@ -19,6 +23,7 @@ export const authReducer = (state = {user: {}}, action) => {
 
         case LOGIN_SUCCESS:
         case REGISTER_USER_SUCCESS:
+        case LOAD_USER_SUCCESS:
             return{
                 ...state,
                 loading: false,
@@ -27,9 +32,16 @@ export const authReducer = (state = {user: {}}, action) => {
             }
 
         case LOGIN_FAIL:
-        case REGISTER_USER_FAIL:
+        case REGISTER_USER_FAIL:     
             return {
                 ...state,
+                loading: false,
+                isAuthenticated: false,
+                user: null,
+                error: action.payload
+            }
+        case LOAD_USER_FAIL:
+            return{
                 loading: false,
                 isAuthenticated: false,
                 user: null,
