8d3c40c6453a880f2ac39f2a3fa371937593c3a9
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-04-13 11:19:05 +0300

    The delete from cart function was successfully added.

--- frontend/src/components/Cart/Cart.js
+++ frontend/src/components/Cart/Cart.js
@@ -5,7 +5,7 @@ import MetaData from '../layout/MetaData'
 import { useAlert } from 'react-alert'
 import { useDispatch, useSelector } from 'react-redux'
 import { getProductDetails, clearErrors } from '../../actions/productAction'
-import { addItemToCart } from '../../actions/cartActions'
+import { addItemToCart, removeItemFromCart } from '../../actions/cartActions'
 
 
 const Cart = () => {
@@ -14,25 +14,29 @@ const Cart = () => {
 
     const { cartItems } = useSelector(state => state.cart)
 
+    const removeItemFromCartHandler = (id) => {
+        dispatch(removeItemFromCart(id))
+    }
+
     const increaseQty = (id, quantity, stock) => {
         const newQty = quantity + 1;
-    
-        if(newQty > stock) return;
-    
-    
+
+        if (newQty > stock) return;
+
+
         dispatch(addItemToCart(id, newQty))
-      }
-    
-      const decreaseQty = (id, quantity) => {
-    
+    }
+
+    const decreaseQty = (id, quantity) => {
+
         const newQty = quantity - 1;
-    
-        if(newQty <= 0) return;
-    
-    
+
+        if (newQty <= 0) return;
+
+
         dispatch(addItemToCart(id, newQty))
-        
-      }
+
+    }
 
     return (
         <Fragment>
@@ -44,42 +48,42 @@ const Cart = () => {
                         <div className="col-12 col-lg-8">
                             {cartItems.map(item => (
                                 <Fragment>
-                                    <hr/>
-                                    <div className="cart-item">
-                                <div className="row">
-                                    <div className="col-4 col-lg-3">
-                                        <img src={item.image} alt="Laptop" height="90" width="115" />
-                                    </div>
+                                    <hr />
+                                    <div className="cart-item" key={item.product}>
+                                        <div className="row">
+                                            <div className="col-4 col-lg-3">
+                                                <img src={item.image} alt="Laptop" height="90" width="115" />
+                                            </div>
 
-                                    <div className="col-5 col-lg-3">
-                                        <Link to={`/products/${item.product}`}>
-                                            {item.name}
-                                        </Link>
-                                    </div>
+                                            <div className="col-5 col-lg-3">
+                                                <Link to={`/products/${item.product}`}>
+                                                    {item.name}
+                                                </Link>
+                                            </div>
 
 
-                                    <div className="col-4 col-lg-2 mt-4 mt-lg-0">
-                                        <p id="card_item_price">Ksh. {item.price}</p>
-                                    </div>
+                                            <div className="col-4 col-lg-2 mt-4 mt-lg-0">
+                                                <p id="card_item_price">Ksh. {item.price}</p>
+                                            </div>
 
-                                    <div className="col-4 col-lg-3 mt-4 mt-lg-0">
-                                        <div className="stockCounter d-inline">
-                                            <span className="btn btn-danger minus" onClick={ () => decreaseQty(item.product, item.quantity)}>-</span>
-                                            <input type="number" className="form-control count d-inline" value={item.quantity} readOnly />
+                                            <div className="col-4 col-lg-3 mt-4 mt-lg-0">
+                                                <div className="stockCounter d-inline">
+                                                    <span className="btn btn-danger minus" onClick={() => decreaseQty(item.product, item.quantity)}>-</span>
+                                                    <input type="number" className="form-control count d-inline" value={item.quantity} readOnly />
 
-                                            <span className="btn btn-primary plus" onClick={ () => increaseQty(item.product, item.quantity, item.stock)}>+</span>
+                                                    <span className="btn btn-primary plus" onClick={() => increaseQty(item.product, item.quantity, item.stock)}>+</span>
+                                                </div>
+                                            </div>
+
+                                            <div className="col-4 col-lg-1 mt-4 mt-lg-0">
+                                                <i id="delete_cart_item" className="fa fa-trash btn btn-danger" onClick={() => removeItemFromCartHandler(item.product)}></i>
+                                            </div>
+
                                         </div>
                                     </div>
-
-                                    <div className="col-4 col-lg-1 mt-4 mt-lg-0">
-                                        <i id="delete_cart_item" className="fa fa-trash btn btn-danger"></i>
-                                    </div>
-
-                                </div>
-                            </div>
                                 </Fragment>
                             ))}
-                            
+
                             <hr />
                         </div>
 
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART } from "../constants/cartConstants";
+import { ADD_TO_CART, REMOVE_ITEM_CART } from "../constants/cartConstants";
 
 function getInitialState(){
     return{
@@ -11,22 +11,27 @@ function getInitialState(){
 export const cartReducer = (state = getInitialState(), action) => {
     
   switch (action.type){
-      case ADD_TO_CART:
-          const item= action.payload;
+    case ADD_TO_CART:
+        const item= action.payload;
 
-          const isItemExist = state.cartItems.find(i => i.product === item.product)
+        const isItemExist = state.cartItems.find(i => i.product === item.product)
 
-          if(isItemExist){
-              return {
-                  ...state,
-                  cartItems: state.cartItems.map(i => i.product === isItemExist.product ? item : i)
-              }             
-          }else{
-              return{
-                  ...state,
-                  cartItems: [...state.cartItems, item]
-              }
-          }
+        if(isItemExist){
+            return {
+                ...state,
+                cartItems: state.cartItems.map(i => i.product === isItemExist.product ? item : i)
+            }             
+        }else{
+            return{
+                ...state,
+                cartItems: [...state.cartItems, item]
+            }
+        }
+    case REMOVE_ITEM_CART:
+        return {
+            ...state,
+            cartItems: state.cartItems.filter(i => i.product !== action.payload)
+        }
           
       default:
           return state
